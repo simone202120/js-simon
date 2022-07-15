04aa9725395b45088f52b39b39ae2a69b0d4f37c
Simone Fortunato <frtsmn94@gmail.com>
2022-07-15 16:20:51 +0200

    generazione dei vari array e funzioni per controllo se i numeri sono giusti

--- js/main.js
+++ js/main.js
@@ -9,5 +9,49 @@
 5)  CREARE UN ARRAY CON I NUMERI CHE SONO UGUALI
 6)  MOSTRARE ARRAY DI NUMERI INDOVINATI DALL'UTENTE E NUMERO DI ELEMENTI NELL'ARRAY
 
-
  */
+
+let arraynumeri = [];
+let arrayutente = [];
+let numeriuguali =[];
+let numeriGiusti;
+
+generanumeri()
+recuperanumeri()
+controllo()
+
+
+
+
+
+
+function generanumeri(){
+    while(arraynumeri.length < 8){
+        let numero= Math.floor(Math.random() * 101);
+        if(!arraynumeri.includes(numero)){
+            arraynumeri.push(numero);
+            document.getElementById('boxnumeri').innerHTML=arraynumeri;
+            console.log(arraynumeri)
+        }
+    }
+}
+
+function recuperanumeri(){
+    for(i=0; i<8; i++){
+        let numeriutente=parseInt(prompt('inserisci uno alla volta i numeri'));
+        arrayutente.push(numeriutente);
+        console.log(arrayutente)
+    }
+}
+
+function controllo(){
+    for(x=0; x<arraynumeri.length; x++){
+        if(arrayutente.includes(arraynumeri[x])){
+            numeriuguali.push(arraynumeri[x]);
+        }
+        numeriGiusti = numeriuguali.length;
+        console.log(numeriuguali)
+        console.log(numeriGiusti)
+    }
+    
+}
